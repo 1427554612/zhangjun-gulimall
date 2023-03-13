bf5bedffefef9e04a673ffc460b1f20692ae6ed0
zhangjun <1427554612@qq.com>
2023-03-13 23:20:15 +0800

    提交

---
+++ gulimall-product/src/test/java/com/zhangjun/guli/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangjun.guli.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
