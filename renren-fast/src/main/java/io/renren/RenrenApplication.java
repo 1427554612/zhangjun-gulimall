0817c747120bcf1def53b0b1cede1b8328793bee
zhangjun <1427554612@qq.com>
2023-08-15 00:41:47 +0800

    整和gateway网关

--- renren-fast/src/main/java/io/renren/RenrenApplication.java
+++ renren-fast/src/main/java/io/renren/RenrenApplication.java
@@ -14,6 +14,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RenrenApplication {
 
 	public static void main(String[] args) {
